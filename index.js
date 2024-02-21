498ae1dc7118f94d198353f66e869af9b098b244
tomasrobino <tomaserobino@gmail.com>
2024-02-21 21:59:22 +0100

    search results display done

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import { input } from "@inquirer/prompts";
-
+import { input, select } from "@inquirer/prompts";
 
 
 const ID_STRING = "\"ipc-metadata-list-summary-item__t\"";
 
-//const answer = await input({ message: "Search:" });
-const answer = "starship tr"
-const encodedAnswer = encodeURI(answer);
+const searchAnswer = await input({ message: "Search:" });
+const encodedAnswer = encodeURI(searchAnswer);
 let scrapedSearch;
 scrapedSearch = await fetch("https://www.imdb.com/find/?q="+encodedAnswer+"&ref_=nv_sr_sm").then(res => res.text());
-//const idStringIndex = scrapedSearch.indexOf(ID_STRING);
 
 const searchResults = [];
 let indexVal = 0;
 for (let i = 0; i < 5; i++) {
     indexVal = scrapedSearch.indexOf("href", scrapedSearch.indexOf(ID_STRING, indexVal)) + 6;
     let endIndex = scrapedSearch.indexOf("\"", indexVal);
-    searchResults.push("https://www.imdb.com/" + scrapedSearch.substring(indexVal, endIndex));
+    searchResults.push({
+        name: scrapedSearch.substring(endIndex+2,scrapedSearch.indexOf("<", endIndex)),
+        value: "https://www.imdb.com/" + scrapedSearch.substring(indexVal, endIndex)
+    });
 }
 
-console.log(searchResults);
+const resultsAnswer = select({ message: "Select one of the following results:", choices: searchResults })
